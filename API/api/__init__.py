0519884ee5e8fc18e33f4ed6c3a7cae56f400943
dmithamo <dennisbmithamo@gmail.com>
2018-07-20 20:17:12 +0300

    [feature #159101719] : Build out ' PUT /entries/id ' endpoint

--- API/api/__init__.py
+++ API/api/__init__.py
@@ -18,6 +18,7 @@ DIARY = Diary()
 
 
 @api.route('{}/entries'.format(BASE_URL), methods=['GET'])
+@api.route('{}/entries/'.format(BASE_URL), methods=['GET'])
 def fetch_all_entries():
     """
         Responds to a GET request to '/mydiary/api/v1/entries'
@@ -40,50 +41,15 @@ def fetch_all_entries():
 
     # Handle empty all_entries list
     if not all_entries:
-        result = {
-            'message': 'No entries found.'
-        }
+        response = jsonify(message="No entries exist"), 400
+        
     else:
         result = all_entries
-
-    # Serve response as json, along with status code
-    response = jsonify(result)
-    response.status_code = 200
+        # Serve response as json, along with status code
+        response = jsonify(result)
+        response.status_code = 200
     return response
 
-@api.route('{}/entries'.format(BASE_URL), methods=['POST'])
-def add_an_entry():
-    """
-        Responds to a POST request to '/mydiary/api/v1/entries'
-        endpoint
-    """
-    title = request.args.get('title')
-    body = request.args.get('body')
-    tags = request.args.get('tags')
-    tags = tags.split(' ')
-
-    if title and body:
-        entry = DIARY.add_entry(title, body, tags)
-
-        # Handle entry not created
-        if not entry:
-            response = jsonify({'Not created' : 'Similar entry exists'})
-            response.status_code = 400
-        else:
-            response = jsonify({
-                'entry added' : {
-                    'title' : title,
-                    'body' : body,
-                    'tags' : tags
-                }
-            })
-            response.status_code = 201
-
-    else:
-        response = jsonify({'Not created' : 'Title or body missing'})
-        response.status_code = 400
-
-    return response
 
 @api.route('{}/entries/<int:id>'.format(BASE_URL), methods=['GET'])
 def fetch_single_entry(id):
@@ -96,12 +62,11 @@ def fetch_single_entry(id):
     # Handle no id found (result=None)
     if not entry:
         response = jsonify(
-            message="Error. Entry with id '{}' not found".format(id)), 404
+            message="Not found error. Entry with id '{}' not found".format(id)), 404
 
     # Render entry as dict, if one is found
     else:
         result = {
-            'entry_id': entry.entry_id,
             'entry_title': entry.entry_title,
             'entry_body': entry.entry_body,
             'entry_created_on': entry.entry_created_on,
@@ -111,6 +76,99 @@ def fetch_single_entry(id):
         response.status_code = 200
     return response
 
+
+@api.route('{}/entries'.format(BASE_URL), methods=['POST'])
+def add_an_entry():
+    """
+        Responds to a POST request to '/mydiary/api/v1/entries'
+        endpoint
+    """
+    title = request.args.get('title')
+    body = request.args.get('body')
+    tags = request.args.get('tags')
+    if tags:
+        tags = tags.split(' ')
+    else:
+        tags = []
+
+    if title and body:
+        entry = DIARY.add_entry(title, body, tags)
+
+        # Handle entry not created
+        if not entry:
+            response = jsonify(message='Bad request. Similar entry exists'), 400
+        else:
+            response = jsonify({
+                    'title': title,
+                    'body': body,
+                    'entry_created_on' : entry.entry_created_on,
+                    'tags': tags
+                    }
+            )
+            response.status_code = 201
+
+    else:
+        response = jsonify(message='Bad request. Title or body missing')
+        response.status_code = 400
+
+    return response
+
+@api.route('{}/entries/<int:id>'.format(BASE_URL), methods=['PUT'])
+def modify_an_entry(id):
+    """
+        Responds to a PUT request to '/mydiary/api/v1/entries/id'
+        endpoint
+    """
+
+    entry = DIARY.get_entry(id)         
+    # Handle entry not found in 'db'
+    if not entry:
+        response = jsonify(message="Not found error. Entry with id '{}' not found".format(id)), 404
+    # If entry with given id is found ...
+    else:
+        title = request.args.get('title')
+        body = request.args.get('body')
+        tags = request.args.get('tags')
+
+        if tags:
+            tags = tags.split(' ')
+        else:
+            tags = []
+        
+        # If request contains ANY of these params
+        if title or body or tags:
+            # Collect entry_properties to change in dict
+            properties_to_edit = {
+                'entry_title' : title,
+                'entry_body' : body,
+                'entry_tags' : tags
+            }
+
+            # If any of these params is different from the entry's params ...
+            #  proced to edit, send 'successful PUT' response
+            if entry.entry_title != title or entry.entry_body != body \
+            or entry.entry_tags != tags:
+
+                DIARY.edit_entry(entry, properties_to_edit)
+
+                response = jsonify({
+                        'title': title,
+                        'body': body,
+                        'entry_created_on' : entry.entry_created_on,
+                        'tags': tags
+                        }
+                )
+                response.status_code = 201
+        
+            # Handle no changes detected
+            else:
+                response = jsonify(message="Bad request. No changes detected"), 400
+        # Handle params to edit not provided
+        else:
+            response = jsonify(message='Bad request. No data provided'), 400
+            response.status_code = 400
+
+    return response
 
 
 if __name__ == '__main__':
