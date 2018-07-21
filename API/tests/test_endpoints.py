b7b625f732bc92abb9a2c1d31ee7063a9dcfe900
dmithamo <dennisbmithamo@gmail.com>
2018-07-21 11:29:37 +0300

    [chore #159102230] : Test POST endpoint

--- API/tests/test_endpoints.py
+++ API/tests/test_endpoints.py
@@ -12,6 +12,19 @@ import config
 # Part of api url common to all endpoints
 BASE_URL = '/mydiary/api/v1'
 
+# Reusable entry_params
+SAMPLE_ENTRY_PARAMS_1 = {
+    'title' : 'Dennis the Me',
+    'body' : 'My name is Dennis and I yam',
+    'tags' : 'tag one too'
+}
+
+SAMPLE_ENTRY_PARAMS_2 = {
+    'title' : 'Who let',
+    'body' : 'Did you let them out',
+    'tags' : 'the dogs'
+}
+
 # Function wide scope - runs once per test
 @pytest.fixture(scope='module') 
 def api_test_client():
@@ -46,3 +59,34 @@ def test_get_entries_2(api_test_client):
     response = api_test_client.get('{}/entries/1'.format(BASE_URL))
     assert response.status_code == 404
     assert 'Not found error. Entry with' in str(response.data)
+
+def test_post_entries_1(api_test_client):
+    """
+        3. Test 'POST /entries' - with all required params present
+    """
+    response = api_test_client.post('{}/entries?title={}&body={}&tags={}'.format(
+        BASE_URL, SAMPLE_ENTRY_PARAMS_1['title'],
+        SAMPLE_ENTRY_PARAMS_1['body'], SAMPLE_ENTRY_PARAMS_1['tags'] 
+        ))
+    assert response.status_code == 201
+    assert 'Dennis the Me' in str(response.data)
+
+def test_post_entries_2(api_test_client):
+    """
+        4. Test 'POST /entries' - with a required param missing
+    """
+    response = api_test_client.post('{}/entries?title={}'.format(
+        BASE_URL, SAMPLE_ENTRY_PARAMS_2['title']
+        ))
+    assert response.status_code == 400
+    assert 'Title or body missing' in str(response.data)
+
+def test_post_entries_3(api_test_client):
+    """
+        5. Test 'POST /entries' - when entry with similar params exists
+    """
+    response = api_test_client.post('{}/entries?title={}&body={}'.format(
+        BASE_URL, SAMPLE_ENTRY_PARAMS_1['title'], SAMPLE_ENTRY_PARAMS_1['body']
+        ))
+    assert response.status_code == 400
+    assert 'Similar entry exists' in str(response.data)
